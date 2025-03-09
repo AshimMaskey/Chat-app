1d37587ce865f3246935af3a671c35da7e499c43
Ashim Maskey <ashimmaskey4@gmail.com>
2025-03-09 22:54:51 +0545

    added generatetoken and finished signup

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,13 @@
 import bcrypt from 'bcryptjs'
+import {generateToken} from '../lib/utils.js'
 import User from '../models/user.model.js'
 export const signup=async(req,res)=>{
 	const {fullName,email,password}=req.body;
 	try {
+		if(!fullName || !email || !password) return res.status(400).json({message: 'All fields are required'});
 		if(password.length<6) return res.status(400).json({message: 'Password length should be more than 6'});
 		
-		const user=await User.find({email});
+		const user=await User.findOne({email});
 		if(user) return res.status(400).json({message: 'Email already exists'});
 
 		//password hashing
@@ -20,13 +22,20 @@ export const signup=async(req,res)=>{
 
 		if(newUser){
 			//generate JWT token
-
+			generateToken(newUser._id,res);
+			await newUser.save();
+			res.status(201).json({
+				_id:newUser._id,
+				email:newUser.email,
+				fullName:newUser.fullName,
+				profilePic:newUser.profilePic
+			})
 		}else{
 			return res.status(400).json({message: "Invalid user data"});
 		}
-
 	} catch (error) {
-		
+		console.log("error in signup controller", error.message);
+		return res.status(500).json({message: "internal server error"});		
 	}
 }
 
