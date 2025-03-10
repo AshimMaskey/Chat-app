134685345ff9aca6c8a2ea5b695d1dca9a38c857
Ashim Maskey <ashimmaskey4@gmail.com>
2025-03-10 10:18:21 +0545

    added login logout and auth middleware to protect routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
@@ -7,6 +8,7 @@ dotenv.config();
 const app=express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.use("/api/auth",authRoutes);
