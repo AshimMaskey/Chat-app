3b4433f502b4b668d46ffc6c2c8fab10d75a778f
Ashim Maskey <ashimmaskey4@gmail.com>
2025-03-10 19:03:39 +0545

    added message related routes and stuffs

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -12,6 +14,7 @@ app.use(cookieParser());
 
 
 app.use("/api/auth",authRoutes);
+app.use("/api/message", messageRoutes);
 
 //creating server
 const PORT=process.env.PORT;
